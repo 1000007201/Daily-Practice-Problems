a4b2d7f09d0474884cef0863b1902b4085cd2d4a
1000007201 <nishant.sharma5504@gmail.com>
2022-03-09 20:15:11 +0530

    [Nishant] Add:Calculated frequency of every character in string

---
+++ String-Problem/Freq_char.py
@@ -0,0 +1,13 @@
+# Desc:
+# Frequency of character in given String
+
+string_ = "Hello, How are you?"
+dict_ = {}
+
+for i in string_:
+    if i not in dict_.keys():
+        dict_[i] = 1
+    else:
+        dict_[i] += 1
+
+print(dict_)
